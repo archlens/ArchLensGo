32799499039356cf1f3826769408215ed9b71027
Slug-Boi <theis.p.holm@gmail.com>
2026-09-21 14:20:22 +0200

    feat: use parsing command with ts_python to get imports from python files + change to sugar logger

--- src/cmd/render.go
+++ src/cmd/render.go
@@ -1,10 +1,12 @@
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/archlens/ArchLens/input"
+	"github.com/archlens/ArchLens/parsers"
 	"github.com/spf13/cobra"
+	"go.uber.org/zap"
+
+	ts_python "github.com/tree-sitter/tree-sitter-python/bindings/go"
 )
 
 // renderCmd represents the render command
@@ -18,43 +20,58 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
+		// probably move this and make it a bit more generalized
+		logger, _ := zap.NewProduction()
+		defer logger.Sync()
+		sugar := logger.Sugar()
+
 		var configPath string
 		if len(args) == 0 {
 			configPath = "archlens.json"
 		} else {
 			configPath = args[0]
 		}
-		
+
 		res, err := input.Load(configPath)
 		if err != nil {
-			fmt.Printf("Error when trying to load configuration: %v\n", err)
+			sugar.Errorf("Error when trying to load configuration: %v", err)
 			return
 		}
-		fmt.Printf("Config: %+v\n", *res)
+		sugar.Infof("Config: %+v", *res)
+
 		viewFiles := make(map[string][]string)
 		for name, view := range res.Views {
 			files, err := input.GetFiles(&view, res.RootFolder)
 			if err != nil {
-				fmt.Printf("Error when trying to get files for %s: %v\n", name, err)
+				sugar.Errorf("Error when trying to get files for %s: %v", name, err)
 			}
 			viewFiles[name] = files
 		}
+
+		var data [][]byte
+		var errs []error
 		for name, files := range viewFiles {
-			fmt.Printf("%s:\t%+s\n", name, files)
+			sugar.Debugf("%s: %d files: %v", name, len(files), files)
+			if len(files) == 0 {
+				continue
+			}
+
+			data, errs = input.ReadFiles(files)
+			if len(data) == 0 || data[0] == nil {
+				continue
+			}
+
+			for _, err := range errs {
+				if err != nil {
+					sugar.Error(err)
+				}
+			}
+			parsers.Parse(ts_python.Language(), data[0])
 		}
+
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(renderCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// renderCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// renderCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
