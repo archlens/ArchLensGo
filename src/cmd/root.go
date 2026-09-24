dd68f85bea793dc7095dbdd32b1f31adb3961fd5
Slug-Boi <theis.p.holm@gmail.com>
2026-09-24 09:44:22 +0200

    refactor: create global sugar logger Sugar for use everywhere

--- src/cmd/root.go
+++ src/cmd/root.go
@@ -4,6 +4,7 @@ import (
 	"os"
 
 	"github.com/spf13/cobra"
+	"go.uber.org/zap"
 )
 
 // rootCmd represents the base command when called without any subcommands
@@ -16,9 +17,18 @@ var rootCmd = &cobra.Command{
 	// Run: func(cmd *cobra.Command, args []string) { },
 }
 
+// Global logger for ease of use
+var Sugar *zap.SugaredLogger
+
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
+	logger, _ := zap.NewProduction()
+	defer func() {
+		_ = logger.Sync()
+	}()
+	Sugar = logger.Sugar()
+
 	err := rootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
